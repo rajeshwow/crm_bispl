d09a35c87209caa2730bf67555a99ac0e427c290
rajesh Prajapat <rprajapat@thb.co.in>
2022-02-09 08:15:20 +0530

    fff

--- renewal.php
+++ renewal.php
@@ -521,8 +521,8 @@ $row[] = $rw;
       <option selected="true" value="20">20</option> 
       <option value="50">50</option>
       <option value="100">100</option>
-      <option value="100">200</option>
-      <option value="100">500</option>
+      <option value="200">200</option>
+      <option value="500">500</option>
       </select>
       <!-- </div> -->
       </li>
