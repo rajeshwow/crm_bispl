64af30f0b741f0a1d2704a00c111efcfd686a8df
rajesh Prajapat <rprajapat@thb.co.in>
2022-05-17 12:52:28 +0530

    fixessss

--- exporttabledatasalesreport.php
+++ exporttabledatasalesreport.php
@@ -35,7 +35,7 @@ while($row = mysqli_fetch_row($result)) {
 	fputcsv($fp, $row);
 }
 // header("Refresh:1; url=./crm_salesreport.php");
-header("location: ./crm_salesreport.php");
+// header("location: ./crm_salesreport.php");
 
 exit;
 
